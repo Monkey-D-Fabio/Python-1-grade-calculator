8255766258f56ce107ba7c14d8cae92e016ee248
Monkey-D-Fabio <141592488+Monkey-D-Fabio@users.noreply.github.com>
2024-09-27 18:48:18 -0400

    Update grade_calc.py

--- grade_calc.py
+++ grade_calc.py
@@ -1,15 +1,15 @@
 # Input for different grades
 
-nbr_lab_problems = input("Enter the number of labs completed: ")
-nbr_quizzes = input("Enter the number of quizzes completed: ")
-grade_assignment_1 = input("Enter grade for Assignment 1: ")
-grade_assignment_2 = input("Enter grade for Assignment 2: ") 
-grade_assignment_3 = input("Enter grade for Assignment 3: ") 
-grade_assignment_4 = input("Enter grade for Assignment 4: ") 
-grade_midterm_1 = input("Enter grade for Midterm 1: ") 
-grade_midterm_2 = input("Enter grade for Midterm 2: ")
-grade_final = input("Enter grade for Final Exam: ")
-grade_midterm_final = input("Enter grade for Midterms and Final Preparation: ")
+nbr_lab_problems = int(input("Enter the number of labs completed: "))
+nbr_quizzes = int(input("Enter the number of quizzes completed: "))
+grade_assignment_1 = int(input("Enter grade for Assignment 1: "))
+grade_assignment_2 = int(input("Enter grade for Assignment 2: "))
+grade_assignment_3 = int(input("Enter grade for Assignment 3: ")) 
+grade_assignment_4 = int(input("Enter grade for Assignment 4: ")) 
+grade_midterm_1 = int(input("Enter grade for Midterm 1: ")) 
+grade_midterm_2 = int(input("Enter grade for Midterm 2: "))
+grade_final = int(input("Enter grade for Final Exam: "))
+grade_midterm_final = int(input("Enter grade for Midterms and Final Preparation: "))
 
 # Weight Labs & Quizzes 
 
@@ -19,7 +19,7 @@ else:
     lab_weight = nbr_lab_problems * (10/3) 
 
 if nbr_quizzes > 6:
-    quizzes_weight = 20
+    quizzes_weight = 15
 else:
     quizzes_weight = nbr_quizzes * (10/3)
 
@@ -33,9 +33,9 @@ weight_midterms = 0.125 * (grade_midterm_1 + grade_midterm_2)
 weight_final = grade_final * 0.18
 weight_midterm_final = grade_midterm_final * 0.06
 
-# Total grade rounded
+# Total grade
 
-total_grade = round(weight_midterm_final + weight_final + weight_midterms + weight_assignments + quizzes_weight + lab_weight)
+total_grade = round(weight_midterm_final + weight_final + weight_midterms + weight_assignments + quizzes_weight + lab_weight, 2)
 
 # Total grade output
 
